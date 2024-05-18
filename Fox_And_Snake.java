ff53a247a73754793f7e3b8b27134fb0bfd752e4
Viram Shah <shahviramhh98@gmail.com>
2024-05-18 16:14:37 +0530

    done

---
+++ Fox_And_Snake.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Fox_And_Snake {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 1) {
+                if (i % 4 == 1) {
+                    for (int j = 0; j < m-1; j++) {
+                        System.out.print(".");
+                    }
+                    System.out.print("#");
+                }else{
+                  
+                    System.out.print("#");
+                    for (int j = 1; j < m; j++) {
+                        System.out.print(".");
+                    }
+                }
+            } else {
+                for (int j = 0; j < m; j++) {
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
