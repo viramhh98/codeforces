4b4e46f75d0083994a10abc25f3a976570f1f0ed
Viram Shah <shahviramhh98@gmail.com>
2024-05-27 04:33:36 +0530

    done

---
+++ Again_Twenty_Five.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Again_Twenty_Five {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        sc.nextLong();
+        sc.close();
+        System.out.println("25");
+    }
+}
