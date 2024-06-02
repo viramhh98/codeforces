30fcaa7900e6cf56d2f7014f4c7c1c089e80eb87
Viram Shah <shahviramhh98@gmail.com>
2024-06-02 18:32:53 +0530

    done

--- Die_Roll.java
+++ Die_Roll.java
@@ -7,17 +7,22 @@ public class Die_Roll {
         int b = sc.nextInt();
         int c=7-Math.max(a, b);
         int d=6;
-        while (true) {
-            if(c%3==0 && d%3==0){
-                c/=3;
-                d/=3;
-            }else if(c%2==0 && d%2==0){
-                c/=2;
-                d/=2;
-            }else{
-                break;
+        if(a==b){
+            System.out.println("1/1");
+        }else{
+
+            while (true) {
+                if(c%3==0 && d%3==0){
+                    c/=3;
+                    d/=3;
+                }else if(c%2==0 && d%2==0){
+                    c/=2;
+                    d/=2;
+                }else{
+                    break;
+                }
             }
+            System.out.println(c+"/"+d);
         }
-        System.out.println(c+"/"+d);
     }
 }
