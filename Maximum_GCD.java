02f02cee5746c8059eb815d6941ee8030c946abc
Viram Shah <shahviramhh98@gmail.com>
2024-06-25 14:51:00 +0530

    done

---
+++ Maximum_GCD.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Maximum_GCD {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int test_case=sc.nextInt();
+        for (int i = 0; i < test_case; i++) {
+            long a=sc.nextInt();
+            System.out.println((int)a/2);
+        }
+    }
+}
