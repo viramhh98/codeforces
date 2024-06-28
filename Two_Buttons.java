f26580f35473ab6900f372f8e920871ae17564fe
Viram Shah <shahviramhh98@gmail.com>
2024-06-28 23:25:34 +0530

    done

--- Two_Buttons.java
+++ Two_Buttons.java
@@ -1,71 +1,20 @@
-import java.util.Scanner;
-
-public class Two_Buttons {
-    static int n=0;
-    static int count=0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Two_Buttons s=new Two_Buttons();
-        n = sc.nextInt();
-        int m = sc.nextInt();
-        count = 0;
-
-        if (n == m) {
-
-        } else if (n > m) {
-            count += n - m;
-        } else {
-           if(m%2==0){
-                count=evenans(m);
-           }else{
-                count=oddans(m);
-           }
-        }
-        System.out.println(count);
-    }
-    // static int evenans(int m){
-    //     int count=0;
-    //     int a =m/2;
-    //     while (n < a) {
-    //         n *= 2;
-    //         count++;
-    //     }
-    //     if (n > a) {
-    //         count += n - a;
-    //         n-=(n-a);
-    //     }
-    //     n*=2;
-    //     return ++count;
-    // }
-    static int evenans(int m){
-        System.out.println("even "+ n + " "+ m + " "+count);
-        if(n==m){
-            return 0;
-        }
-        if(n<Math.ceilDiv(m, 2)){
-            if(m%2==0){
-                count +=evenans(m/2);
-            }else{
-                count+=oddans(m/2);
-            }
-        }
-        n*=2;
-        return count+1;
-    }
-    static int oddans(int m){
-        System.out.println("odd "+ n + " "+ m + " "+count);
-
-        if(n==m){
-            return 0;
-        }if(n<Math.ceilDiv(m, 2)){
-            if(m%2==0){
-               count+= evenans(m/2);
-            }else{
-                count+= oddans(m/2);
-            }
-        }
-        n*=2;
-        n=(n-m);
-        return count+ 1+n;
-    }
+import java.util.*;
+public class Two_Buttons
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int ans=0;
+		while(m>n){
+		    if(m%2==1){
+		        m++;
+		        ans++;
+		    }
+		    m=m/2;
+		    ans++;
+		}
+		ans+=(n-m);
+		System.out.println(ans);
+	}
 }
